2b3e72fe0cafb0c5a769d63b5a540278fac235dc
guilhermesouza11 <guilhermesouzadossantos11@gmail.com>
2023-01-23 11:46:43 -0300

    Terminando a Aula 3

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const nlwSetup = new NLWSetup(form)
 
 const data = {
     run: ['01-01', '01-02', '01-06'],
+    journal: ['01-02', '01-07','01-12'],
+    takePills: ['01-03', '01-04']
 };
 
 nlwSetup.setData(data);
