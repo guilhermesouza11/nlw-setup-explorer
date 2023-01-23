88755289efb0adc4724dc09877f74aa10000bbe3
Guilherme Souza <guilhermesouzadossantos11@gmail.com>
2023-01-23 20:53:06 -0300

    Profissionalizando o projeto

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form)
 
-const data = {
-    run: ['01-01', '01-02', '01-06'],
-    journal: ['01-02', '01-07','01-12'],
-    takePills: ['01-03', '01-04']
-};
+const button = document.querySelector('header button');
 
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add(){
+    
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists){
+        alert("Dia já incluso ❌")
+        return
+    }
+
+    alert("Adicionado com sucesso ✅")
+    nlwSetup.addDay(today)
+}
+
+function save(){
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data);
 nlwSetup.load();
